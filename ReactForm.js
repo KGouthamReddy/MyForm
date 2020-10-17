0214c06629d54a83da3407a1f26d9023a31f4aa8
Goutham Reddy <64308772+KGouthamReddy@users.noreply.github.com>
2020-10-17 10:50:39 +0530

    Update ReactForm.js

--- ReactForm.js
+++ ReactForm.js
@@ -2,39 +2,48 @@ import React, { useState } from 'react';
 
 
 const App=()=>{
-    const [name ,setName] = useState("");
-    const [fullName , setFullName] = useState();
-    const [lastname , setLastname] = useState("");
-    const [LastName , setLastName] = useState();
+    const [fullName ,setFullName] = useState({
+        fname: "",
+        lname: "",
+        email: "",
+        phone: "",
+        college:"",
     
-
-    const onSubmit =(event)=>{
-        event.preventDefault();
-        setFullName(name)
-        setLastName(lastname)
-    }
+    });
     const inputEvent=(event)=>
     {
         console.log(event.target.value);
-        setName(event.target.value)
-    }
-    const Event=(event)=>
-    {
-        console.log(event.target.value);
-        setLastname(event.target.value)
-    }
+        console.log(event.target.name);
 
+        const {value,name} = event.target;
+         setFullName((preValue) =>{
+             console.log(preValue)
+             return{
+                 ...preValue,
+                 [name]:value,
+             }
+     });
+      
+    };
+        const onSubmit =(event)=>{
+        event.preventDefault();
+        alert("form submitted");
+        }
     
     
-    
-
     return(
         <>
         <div>
         <form>
-            <h1>Hello! {fullName} {LastName}</h1>
-            <input type="text" placeholder="Enter Your First Name" onChange={inputEvent} value={name}></input>
-            <input type="text" placeholder="Enter Your Last Name" onChange={Event} value={lastname}></input>
+            <h1><b>Hello! </b>{fullName.fname} {fullName.lname}</h1>
+            <p><b>{fullName.college}</b></p>
+            <p><b>{fullName.email}</b></p>
+            <p><b>{fullName.phone}</b></p>
+            <input type="text" placeholder="Enter Your First Name" name="fname" onChange={inputEvent} value={fullName.fname}></input> <br/>
+            <input type="text" placeholder="Enter Your Last Name" name="lname" onChange={inputEvent} value={fullName.lname}></input> <br/>
+            <input type="text" placeholder="Enter Your College Name" name="college" onChange={inputEvent} value={fullName.college}></input> <br/>
+            <input type="email" placeholder="Enter Your Gmail" name="email" onChange={inputEvent} value={fullName.email}></input> <br/>
+            <input type="number" placeholder="Enter Your Mobile" name="phone" onChange={inputEvent} value={fullName.phone}></input> <br/>
             <button onClick={onSubmit}>SUBMIT HERE</button>
         </form>
         </div>
